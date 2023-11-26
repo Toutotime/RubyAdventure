ec730942eae53921e7841f470082e743114641a0
tophatbadger <108828245+tophatbadger@users.noreply.github.com>
2023-11-25 23:06:45 -0500

    Updated enemies, new particle systems added, minor cosmetic additions

--- Assets/2DBeginnerTutorialResources/Scripts/EnemyController.cs
+++ Assets/2DBeginnerTutorialResources/Scripts/EnemyController.cs
@@ -65,14 +65,14 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
-            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move X", 1);
             animator.SetFloat("Move Y", direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", direction);
-            animator.SetFloat("Move Y", 0);
+            animator.SetFloat("Move Y", 1);
         }
         
         rigidbody2D.MovePosition(position);
